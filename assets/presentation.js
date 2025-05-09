ca29cc7269db9100f03aafcf699ab3ac042b2970
Jason Cairns <jason@cair.nz>
2025-05-09 23:39:46 +1200

    init

---
+++ assets/presentation.js
@@ -0,0 +1,5 @@
+import Reveal from '../node_modules/reveal.js/dist/reveal.esm.js';
+
+let deck = new Reveal();
+deck.initialize();
+
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default =
+        with pkgs;
+        mkShell {
+          buildInputs = [
+            nodejs
+            importNpmLock.hooks.linkNodeModulesHook
+            superhtml
+            typescript-language-server
+          ];
+
+          npmDeps = importNpmLock.buildNodeModules {
+            npmRoot = ./.;
+            inherit nodejs;
+          };
+        };
+    };
+}
