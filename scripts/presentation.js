4319e1fb5cfd0dd184bf0368ed82a2415b07abf6
Jason Cairns <jason@cair.nz>
2025-05-18 17:27:46 +1200

    dev: reshuffle reveal imports

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -3,9 +3,9 @@ import RevealMarkdown from 'reveal.js/plugin/markdown/markdown'
 import RevealNotes from 'reveal.js/plugin/notes/notes'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
 import RevealMath from 'reveal.js/plugin/math/math'
+import 'reveal.js/plugin/highlight/zenburn.css'
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/serif.css';
-import 'reveal.js/plugin/highlight/zenburn.css'
 
 fetch('assets/comparison-table.html')
       .then(response => response.text())
