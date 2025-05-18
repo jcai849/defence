19d02fa7035f8680e92b291474c2d49345587d43
Jason Cairns <jason@cair.nz>
2025-05-18 15:46:13 +1200

    feat: include dLASSO

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -2,8 +2,10 @@ import Reveal from 'reveal.js';
 import RevealMarkdown from 'reveal.js/plugin/markdown/markdown'
 import RevealNotes from 'reveal.js/plugin/notes/notes'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
+import RevealMath from 'reveal.js/plugin/math/math'
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/serif.css';
+import 'reveal.js/plugin/highlight/zenburn.css'
 
 fetch('assets/comparison-table.html')
       .then(response => response.text())
@@ -13,7 +15,7 @@ fetch('assets/comparison-table.html')
 
 let deck = new Reveal();
 deck.initialize({
-  plugins: [RevealMarkdown, RevealNotes, RevealHighlight],
+  plugins: [RevealMarkdown, RevealNotes, RevealHighlight, RevealMath.KaTeX],
   totalTime: 1200,
   hash: true,
   center: false
